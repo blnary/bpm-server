f469901ffa4a07cc59208c377cc2215f541ce092
5eqn <491100866@qq.com>
2023-06-28 08:29:46 +0800

    Make minor optimizations

--- bpm_detection/bpm_detection.py
+++ bpm_detection/bpm_detection.py
@@ -2,8 +2,8 @@
 #
 # This file is part of The BPM Detector Python
 #
-# The BPM Detector Python is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# The BPM Detector Python is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
 #
@@ -18,38 +18,29 @@
 # Boston, MA 02110-1301, USA.
 
 import argparse
+import librosa
 import array
 import math
-import wave
+from pydub import AudioSegment
 
 import matplotlib.pyplot as plt
 import numpy
 import pywt
 from scipy import signal
+from tqdm import tqdm
 
 
-def read_wav(filename):
-    # open file, get metadata for audio
+def read_mp3(filename):
+    # Load the MP3 file
     try:
-        wf = wave.open(filename, "rb")
-    except IOError as e:
+        audio = AudioSegment.from_mp3(filename)
+    except Exception as e:
         print(e)
-        return
+        return None, None
 
-    # typ = choose_type( wf.getsampwidth() ) # TODO: implement choose_type
-    nsamps = wf.getnframes()
-    assert nsamps > 0
-
-    fs = wf.getframerate()
-    assert fs > 0
-
-    # Read entire file and make into an array
-    samps = list(array.array("i", wf.readframes(nsamps)))
-
-    try:
-        assert nsamps == len(samps)
-    except AssertionError:
-        print(nsamps, "not equal to", len(samps))
+    # Extract audio data and sample rate
+    samps = array.array("i", audio.raw_data)
+    fs = audio.frame_rate
 
     return samps, fs
 
@@ -62,11 +53,7 @@ def no_audio_data():
 
 # simple peak detection
 def peak_detect(data):
-    max_val = numpy.amax(abs(data))
-    peak_ndx = numpy.where(data == max_val)
-    if len(peak_ndx[0]) == 0:  # if nothing found then the max must be negative
-        peak_ndx = numpy.where(data == -max_val)
-    return peak_ndx
+    return numpy.argmax(data)
 
 
 def bpm_detector(data, fs):
@@ -90,78 +77,92 @@ def bpm_detector(data, fs):
             [cA, cD] = pywt.dwt(cA, "db4")
 
         # 2) Filter
-        cD = signal.lfilter([0.01], [1 - 0.99], cD)
+        cD = signal.lfilter([0.01], [1 - 0.99], cD)[0: math.floor(cD_minlen)]
 
         # 4) Subtract out the mean.
 
         # 5) Decimate for reconstruction later.
-        cD = abs(cD[:: (2 ** (levels - loop - 1))])
+        cD = abs(cD)
         cD = cD - numpy.mean(cD)
 
         # 6) Recombine the signal before ACF
-        #    Essentially, each level the detail coefs (i.e. the HPF values) are concatenated to the beginning of the array
-        cD_sum = cD[0 : math.floor(cD_minlen)] + cD_sum
+        #    Essentially, each level the detail coefs (i.e. the HPF values)
+        #    are concatenated to the beginning of the array
+        cD_sum = cD + cD_sum
 
     if [b for b in cA if b != 0.0] == []:
         return no_audio_data()
 
     # Adding in the approximate data as well...
-    cA = signal.lfilter([0.01], [1 - 0.99], cA)
+    cA = signal.lfilter([0.01], [1 - 0.99], cA)[0: math.floor(cD_minlen)]
     cA = abs(cA)
     cA = cA - numpy.mean(cA)
-    cD_sum = cA[0 : math.floor(cD_minlen)] + cD_sum
+    cD_sum = cA + cD_sum
 
     # ACF
     correl = numpy.correlate(cD_sum, cD_sum, "full")
 
-    midpoint = math.floor(len(correl) / 2)
-    correl_midpoint_tmp = correl[midpoint:]
-    peak_ndx = peak_detect(correl_midpoint_tmp[min_ndx:max_ndx])
-    if len(peak_ndx) > 1:
-        return no_audio_data()
+    midpoint = len(correl) // 2
+    correl_midpoint_tmp = correl[midpoint + min_ndx:midpoint + max_ndx]
+    mult = numpy.arange(min_ndx, max_ndx) / max_ndx
+    correl_midpoint_tmp = correl_midpoint_tmp * mult
+    correl_midpoint_tmp = correl_midpoint_tmp / \
+        numpy.linalg.norm(correl_midpoint_tmp)
 
-    peak_ndx_adjusted = peak_ndx[0] + min_ndx
+    # correl_midpoint_tmp = numpy.convolve(
+    #     correl_midpoint_tmp, [-1, 1], mode='same')
+    peak_ndx = peak_detect(correl_midpoint_tmp)
+    peak_ndx_adjusted = peak_ndx + min_ndx
     bpm = 60.0 / peak_ndx_adjusted * (fs / max_decimation)
-    print(bpm)
-    return bpm, correl
+
+    # Get seconds per beat
+    n = numpy.arange(0, len(correl_midpoint_tmp))
+    n = (n + min_ndx) / (fs / max_decimation)
+    plt.plot(n, correl_midpoint_tmp)
+    return bpm, correl_midpoint_tmp[peak_ndx]
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process .wav file to determine the Beats Per Minute.")
-    parser.add_argument("--filename", required=True, help=".wav file for processing")
+    parser = argparse.ArgumentParser(
+        description="Process .mp3 file to determine the Beats Per Minute.")
+    parser.add_argument("--filename", required=True,
+                        help=".mp3 file for processing")
     parser.add_argument(
         "--window",
         type=float,
         default=3,
-        help="Size of the the window (seconds) that will be scanned to determine the bpm. Typically less than 10 seconds. [3]",
+        help="Size of the the window (seconds) that will be scanned to \
+                determine the bpm. Typically less than 10 seconds. [3]",
     )
 
     args = parser.parse_args()
-    samps, fs = read_wav(args.filename)
+    samps, fs = read_mp3(args.filename)
     data = []
-    correl = []
+    max_correl = 0
     bpm = 0
+    bpm_by_max = 0
     n = 0
     nsamps = len(samps)
     window_samps = int(args.window * fs)
     samps_ndx = 0  # First sample in window_ndx
     max_window_ndx = math.floor(nsamps / window_samps)
-    bpms = numpy.zeros(max_window_ndx)
 
     # Iterate through all windows
-    for window_ndx in range(0, max_window_ndx):
+    for window_ndx in tqdm(range(0, max_window_ndx)):
 
         # Get a new set of samples
         # print(n,":",len(bpms),":",max_window_ndx_int,":",fs,":",nsamps,":",samps_ndx)
-        data = samps[samps_ndx : samps_ndx + window_samps]
+        data = samps[samps_ndx: samps_ndx + window_samps]
         if not ((len(data) % window_samps) == 0):
             raise AssertionError(str(len(data)))
 
-        bpm, correl_temp = bpm_detector(data, fs)
+        bpm, correl = bpm_detector(data, fs)
         if bpm is None:
             continue
-        bpms[window_ndx] = bpm
-        correl = correl_temp
+
+        if correl > max_correl:
+            max_correl = correl
+            bpm_by_max = bpm
 
         # Iterate at the end of the loop
         samps_ndx = samps_ndx + window_samps
@@ -169,9 +170,11 @@ if __name__ == "__main__":
         # Counter for debug...
         n = n + 1
 
-    bpm = numpy.median(bpms)
-    print("Completed!  Estimated Beats Per Minute:", bpm)
+    while bpm_by_max < 120:
+        bpm_by_max *= 2
+    while bpm_by_max >= 240:
+        bpm_by_max /= 2
+    bpm_by_max = round(bpm_by_max * 2) / 2
+    print("Completed!  Estimated Beats Per Minute:", bpm_by_max)
 
-    n = range(0, len(correl))
-    plt.plot(n, abs(correl))
     plt.show(block=True)
